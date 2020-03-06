2eab838801435ca2946b7848017228f9a3a7fb4f
Jake VanderPlas <jakevdp@google.com>
2020-03-06 08:09:49 -0800

    isolate to vega-lite

--- test.py
+++ test.py
@@ -6,9 +6,7 @@ html = """
 <html>
 <head>
   <title>Embedding Vega-Lite</title>
-  <script src="https://cdn.jsdelivr.net/npm/vega@5.9.2"></script>
   <script src="https://cdn.jsdelivr.net/npm/vega-lite@4.0.2"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.2.2"></script>
 </head>
 <body>
   <div id="vis"></div>
